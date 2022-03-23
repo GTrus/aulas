b63c454822e59945e195f8ee8897287efec5d0c9
GTrus <80901104+GTrus@users.noreply.github.com>
2022-03-22 22:04:39 -0300

    Create codigo.ino

---
+++ Exercícios/exercício 4/codigo.ino
@@ -0,0 +1,23 @@
+const int botao1 = 2;
+const int led = 4;
+
+int var1;
+  
+ void setup()
+ { 
+    pinMode(botao1, INPUT);
+    pinMode(led, OUTPUT);
+   	Serial.begin(9600);
+ }
+ 
+ void loop()
+ {
+    var1 = digitalRead(botao1);
+   	
+   if(var1){
+     Serial.println("botao 1 apertado");
+     digitalWrite(led, HIGH);
+     delay(3000);
+     digitalWrite(led, LOW);
+   }
+ }
